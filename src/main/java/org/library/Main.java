333e40f656b7868bfb6e602e013e1cc4ef084d21
eastl4 <126170011+eastl4@users.noreply.github.com>
2025-07-28 08:49:19 +0700

    Initial commit

--- src/main/java/org/library/Main.java
+++ src/main/java/org/library/Main.java
@@ -1,4 +1,4 @@
-//package org.library;
+package org.library;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
 
-            Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/org/library/Login.fxml"));
             Scene scene = new Scene(root);
 
 
