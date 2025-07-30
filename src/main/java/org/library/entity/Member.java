58857303f02aee31079c534d88a9c06724eaa729
nhatquangle2210 <83912400+nhatquangle2210@users.noreply.github.com>
2025-07-30 09:23:18 +0700

    Add entity class for Member

---
+++ src/main/java/org/library/entity/Member.java
@@ -0,0 +1,98 @@
+package org.library.entity;
+
+public class Member {
+    private int memberID;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String address;
+    private String email;
+    private String contactNumber;
+
+    public Member(int memberID, String firstName, String lastName, String dateOfBirth,
+                  String address, String email, String contactNumber) {
+        this.memberID = memberID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.email = email;
+        this.contactNumber = contactNumber;
+    }
+
+    public Member(String firstName, String lastName, String dateOfBirth, String address, String email, String contactNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.email = email;
+        this.contactNumber = contactNumber;
+    }
+
+    public Member() {
+    }
+
+    // Getters và Setters
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return memberID == member.memberID;
+    }
+}
